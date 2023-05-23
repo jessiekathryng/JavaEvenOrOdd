701b808b162dbd83d6d97d25cbfbda2c13e8e7fa
JessKG <93571235+jessiekathryng@users.noreply.github.com>
2023-05-23 12:55:29 -0500

    Update EvenOrOdd.java

--- EvenOrOdd.java
+++ EvenOrOdd.java
@@ -9,7 +9,7 @@ public class EvenOrOdd {
 //		we will use scanner to point to System.in so we can receive user input; We are assigning it to a local variable named Scanner
 //		we are creating a 'try' block in order to test for errors while being executed; This also means we do not have to close the scanner 
 		try (Scanner scanner = new Scanner(System.in);){
-//		This is when the scanner will start reading the user inputs as integers;We are assigning this to an int variable called num
+//		This is when the scanner will start reading the user inputs as integers; We are assigning this to an int variable called num
 			int num = scanner.nextInt();
 			if (num <= 0) {
 				System.out.println("Invalid Number " + num);
